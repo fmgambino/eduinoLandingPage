9cd548d7eff6a2b9857f92a00445e9c0c8de8e79
Fernando Gambino <fernando.m.gambino@gmail.com>
2023-12-03 15:07:46 +0000

    Add Play List Tutoriales Eduino32 Lite

---
+++ assets/js/video-list.js
@@ -0,0 +1,67 @@
+let allVideos = [
+   {
+      name: "Bienvenidos #EDUINEROS.",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_2"
+   },
+   {
+      name: "Confirm Password using Html CSS & js",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_3"
+   },
+   {
+      name: "Creative Card Hover Effect",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_4"
+   },
+   {
+      name: "Creative Digital Clock Ui Design",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_5"
+   },
+   {
+      name: "Creative Our Team Section",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_6"
+   },
+   {
+      name: "Filter Text Animation Using Html and CSS",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_7"
+   },
+   {
+      name: "Glassmorphism Calculater New Design",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_8"
+   },
+   {
+      name: "How To  Make Cheatsheet",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_9"
+   },
+   {
+      name: "How to create Login Form",
+      src: "https://www.youtube.com/embed/okznQk1Cz6E?si=QGWnCoRxPNzQA39Q",
+      id: "vid_10"
+   },
+   {
+      name: "How To Make Animated Menu Icon",
+      src: "How To Make Animated Menu Icon Using Html",
+      id: "vid_11"
+   },
+   {
+      name: "How to make Read More Function",
+      src: "How to make Read More Function using Html",
+      id: "vid_12"
+   },
+   {
+      name: "Vertical Navigation Bar",
+      src: "Vertical Navigation Bar Using Html And CSS",
+      id: "vid_13"
+   }
+]
